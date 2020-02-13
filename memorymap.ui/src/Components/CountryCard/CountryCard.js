49ab68f88ee2f1a98f61ab598f1ec944040d5416
David Mullins <Mullins6323@gmail.com>
2020-02-13 00:00:14 -0600

    country card pics resize based on image dimensions

--- memorymap.ui/src/Components/CountryCard/CountryCard.js
+++ memorymap.ui/src/Components/CountryCard/CountryCard.js
@@ -6,7 +6,7 @@ class CountryCard extends React.Component {
   render() {
     const { marker } = this.props;
     return (
-          <div className="col-lg-2 card-details">
+          <div className="card-details">
             {marker.image?<img className="country-picture" alt="in country" src={`data:image/jpg;base64,${marker.image}`}/>:<img className="country-picture need-pics" alt="in country" src={needPics}/>}
             <p className="country-description">{marker.description}</p>
           </div>
